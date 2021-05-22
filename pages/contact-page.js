6be07a425c6118bea5bff394518e95b6251049af
takumi <53643382+takumi1005@users.noreply.github.com>
2021-05-22 09:51:48 +0900

    changed to function

--- pages/contact-page.js
+++ pages/contact-page.js
@@ -1,7 +1,7 @@
 import Layout from '../components/Layout'
 import Image from 'next/image'
 
-const Blog = () => {
+export default function Contact() {
   return (
     <Layout title="Contact">
       <div className="bg-white text-center shadow-xl p-8 w-80 rounded">
@@ -80,6 +80,3 @@ const Blog = () => {
     </Layout>
   )
 }
-
-export default Blog
-
